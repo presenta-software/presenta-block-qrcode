073d6b85f541434a54832fd9fe302e829667c41e
abusedmedia <hello@fabiofranchino.com>
2021-09-06 14:15:55 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# PRESENTA QRCode Block
+
+This block render a QRCode in a PRESENTA Library project.
+
+## Installation
+
+Please read the installation istructions for official plugins [here](https://lib.presenta.cc/extend/#install-an-official-plugin) using this unique identifier: `block-qrcode`
+
+## Usage
+
+To configure this block use this setting:
+
+```js
+{
+    type: 'qrcode',
+    content: 'John',
+    color: 'red'
+}
+```
+
+
+## Development
+
+Run the watcher:
+
+    npm start
+
+and the local webserver
+
+    npm run test
+
---
+++ src/index.js
@@ -0,0 +1,42 @@
+import css from './style.css'
+import QRCode from '../node_modules/qrcode/build/qrcode.js'
+
+const block = function (el, config) {
+  const child = document.createElement('div')
+  child.classList.add(css.qrcode)
+
+  const content = config.content
+  const color = config.color
+
+  el.appendChild(child)
+
+  if (content) {
+    if (color) {
+      child.style.setProperty('--qrcodeColor', color)
+    }
+
+    QRCode.toString(content, { type: 'svg', color: { dark: '#000000', light: '#00000000' } }, function (err, str) {
+      if (err) console.error(err)
+      child.innerHTML = str
+    })
+  }
+
+  this.beforeDestroy = () => {
+  }
+
+  this.stepForward = step => {
+  }
+
+  this.destroy = () => {
+  }
+}
+
+export default block
+
+block.install = Presenta => {
+  Presenta.addBlock('qrcode', block)
+}
+
+if (typeof window !== 'undefined' && window.Presenta) {
+  window.Presenta.use(block)
+}
